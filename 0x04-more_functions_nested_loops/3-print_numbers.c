df31a05c7d98350297a2dcad64936cd2128e5d3a
salma017 <salimatahiri17@gmail.com>
2023-04-28 16:46:48 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,17 @@
 #include "main.h"
+
 /**
- *mul - multiply 2 int
- *@a: first int
- *@b: second int
- *Return: addition of the multiplicattion
+ * print_numbers - print 0-9
+ * return: void
  */
 
-int mul(int a, int b)
+void print_numbers(void)
 {
-		return (a * b);
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
